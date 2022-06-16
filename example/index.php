391ddcb3378a400f0b1a33f5d4612606d91e00ee
Ricardo Boss <contact@ricardoboss.de>
2022-06-16 10:35:35 +0200

    Moved title to document meta

--- example/index.php
+++ example/index.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Elephox\Templar\BuildWidget;
+use Elephox\Templar\DocumentMeta;
 use Elephox\Templar\Foundation\Center;
 use Elephox\Templar\Foundation\FullscreenBody;
 use Elephox\Templar\Foundation\FullscreenDocument;
@@ -15,13 +16,15 @@ require_once '../vendor/autoload.php';
 class MyApp extends BuildWidget {
 	protected function build(): Widget {
 		return new FullscreenDocument(
-			head: new Head(
-				title: 'My App',
-			),
+			head: new Head(),
 			body: new FullscreenBody(
 				child: new Center(
 					child: new Text('Hello, world!'),
 				),
+			),
+			documentMeta: new DocumentMeta(
+				title: 'My App',
+				language: 'en-US',
 			),
 		);
 	}
