27bd80b44eda391479a4b6ca916c50a15a9ee689
Ricardo Boss <contact@ricardoboss.de>
2022-06-16 10:35:18 +0200

    Added lang attribute

--- src/Foundation/Document.php
+++ src/Foundation/Document.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Elephox\Templar\Foundation;
 
+use Elephox\Templar\DocumentMeta;
 use Elephox\Templar\HtmlRenderWidget;
 use Elephox\Templar\RenderContext;
 
@@ -10,15 +11,26 @@ class Document extends HtmlRenderWidget {
 	public function __construct(
 		private readonly Head $head,
 		private readonly Body $body,
+		public ?DocumentMeta $documentMeta = null,
 	) {
 	}
 
 	public function render(RenderContext $context): string {
+		if ($this->documentMeta !== null) {
+			$context->documentMeta = $this->documentMeta;
+		}
+
 		return '<!DOCTYPE html>' . parent::render($context);
 	}
 
 	protected function getTag(): string {
 		return 'html';
+	}
+
+	protected function getAttributes(RenderContext $context): array {
+		return parent::getAttributes($context) + [
+				'lang' => $context->documentMeta?->language,
+			];
 	}
 
 	protected function renderChild(RenderContext $context): string {
