3c01728452d915a11143e2f795905252ae9e7638
Ricardo Boss <contact@ricardoboss.de>
2022-06-18 14:55:41 +0200

    Tests for compound length

--- src/CompoundLength.php
+++ src/CompoundLength.php
@@ -3,40 +3,48 @@ declare(strict_types=1);
 
 namespace Elephox\Templar;
 
-class CompoundLength implements EmittableLength {
-	protected array $lengths;
+use Stringable;
 
+class CompoundLength implements EmittableLength {
+	protected array $lengths = [];
+
+	/**
+	 * @param iterable<mixed, Stringable|int|float|string|EmittableLength> $lengths
+	 */
 	public function __construct(
-		array $lengths,
+		iterable $lengths,
 		protected readonly MathOperator $operator,
 	) {
 		foreach ($lengths as $length) {
-			if ($length instanceof self) {
-				if ($length->operator === $this->operator) {
-					foreach ($length->lengths as $innerLength) {
-						$this->lengths[] = $innerLength;
-					}
-				} else {
-					$this->lengths[] = "($length)";
-				}
-			} else {
-				$this->lengths[] = $length;
-			}
+			$this->concat($length);
 		}
 	}
 
 	public function concat(EmittableLength $length): void {
-		$this->lengths[] = $length;
+		if ($length instanceof self) {
+			if ($length->operator === $this->operator) {
+				foreach ($length->lengths as $innerLength) {
+					$this->lengths[] = $innerLength;
+				}
+			} else {
+				$this->lengths[] = "($length)";
+			}
+		} else {
+			$this->lengths[] = $length;
+		}
 	}
 
 	public function __toString(): string {
-		return implode(' ' . $this->operator->value . ' ', array_filter($this->lengths));
+		return implode(
+			' ' . $this->operator->value . ' ',
+			array_filter($this->lengths)
+		);
 	}
 
 	public function toEmittable(): string {
 		$lengths = array_filter($this->lengths);
 		if (count($lengths) === 1) {
-			return (string)$lengths[0];
+			return (string) $lengths[0];
 		}
 
 		return "calc($this)";
---
+++ test/Unit/CompoundLengthTest.php
@@ -0,0 +1,126 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Templar\Tests\Unit;
+
+use Elephox\Templar\CompoundLength;
+use Elephox\Templar\Length;
+use Elephox\Templar\MathOperator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Elephox\Templar\CompoundLength
+ * @covers \Elephox\Templar\Length
+ * @covers \Elephox\Templar\Value
+ * @covers \Elephox\Templar\Templar::combineHashCodes
+ * @uses   \Elephox\Templar\EnumStringable
+ * @uses   \Elephox\Templar\HasEnumHashCode
+ */
+class CompoundLengthTest extends TestCase {
+	public function testToString(): void {
+		$compound =
+			new CompoundLength(
+				[Length::inPx(1)],
+				MathOperator::Plus
+			);
+
+		static::assertSame(
+			'1px',
+			(string) $compound
+		);
+		static::assertSame(
+			'1px',
+			$compound->toEmittable()
+		);
+
+		$compound->concat(Length::inPx(2));
+
+		static::assertSame(
+			'1px + 2px',
+			(string) $compound
+		);
+		static::assertSame(
+			'calc(1px + 2px)',
+			$compound->toEmittable()
+		);
+
+		$compound->concat(Length::inRem(2));
+
+		static::assertSame(
+			'1px + 2px + 2rem',
+			(string) $compound
+		);
+		static::assertSame(
+			'calc(1px + 2px + 2rem)',
+			$compound->toEmittable()
+		);
+
+		$compound->concat(
+			new CompoundLength(
+				[Length::inPx(3), Length::inRem(4)],
+				MathOperator::Minus
+			)
+		);
+
+		static::assertSame(
+			'1px + 2px + 2rem + (3px - 4rem)',
+			(string) $compound
+		);
+		static::assertSame(
+			'calc(1px + 2px + 2rem + (3px - 4rem))',
+			$compound->toEmittable()
+		);
+
+		$compound->concat(
+			new CompoundLength(
+				[Length::inPx(5), Length::inRem(6)],
+				MathOperator::Plus
+			)
+		);
+
+		static::assertSame(
+			'1px + 2px + 2rem + (3px - 4rem) + 5px + 6rem',
+			(string) $compound
+		);
+		static::assertSame(
+			'calc(1px + 2px + 2rem + (3px - 4rem) + 5px + 6rem)',
+			$compound->toEmittable()
+		);
+	}
+
+	public function testGetHashCode(): void {
+		$a =
+			new CompoundLength(
+				[Length::inPx(1)],
+				MathOperator::Plus
+			);
+		$b =
+			new CompoundLength(
+				[Length::inPx(1)],
+				MathOperator::Plus
+			);
+		$c =
+			new CompoundLength(
+				[Length::inPx(2)],
+				MathOperator::Plus
+			);
+		$d =
+			new CompoundLength(
+				[Length::inPx(1)],
+				MathOperator::Minus
+			);
+
+		static::assertSame(
+			$a->getHashCode(),
+			$b->getHashCode()
+		);
+		static::assertNotSame(
+			$a->getHashCode(),
+			$c->getHashCode()
+		);
+		static::assertNotSame(
+			$a->getHashCode(),
+			$d->getHashCode()
+		);
+	}
+}
